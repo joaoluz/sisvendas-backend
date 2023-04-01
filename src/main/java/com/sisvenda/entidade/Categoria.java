c694bbd26007e94d4b85576fcb8b138e67fd7789
João Batista da Luz Spinola <jbspinola@stefanini.com>
2023-04-01 11:58:10 -0300

    Removendo lombok

--- src/main/java/com/sisvenda/entidade/Categoria.java
+++ src/main/java/com/sisvenda/entidade/Categoria.java
@@ -1,18 +1,16 @@
 package com.sisvenda.entidade;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @Table(name = "tab_categoria")
 public class Categoria {
 	
@@ -26,5 +24,29 @@ public class Categoria {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="id_categoria_pai", referencedColumnName="id")
 	private Categoria categoriaPai;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
+	}
+
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
+	}
 }
 	
--- src/main/java/com/sisvenda/exception/ErrorResponse.java
+++ src/main/java/com/sisvenda/exception/ErrorResponse.java
@@ -1,15 +1,36 @@
 package com.sisvenda.exception;
 
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
-@AllArgsConstructor
 public class ErrorResponse {
 
-    private final String message;
+	private final String message;
     private final int code;
     private final String status;
     private final String objectName;
     private final List<ObjectError> errors;
+    
+    public ErrorResponse(String message, int code, String status, String objectName2, List<ObjectError> errors2) {
+    	this.message = message;
+    	this.code = code;
+    	this.status = status;
+    	this.objectName = objectName2;
+    	this.errors = errors2;
+	}
+    
+	public String getMessage() {
+		return message;
+	}
+	public int getCode() {
+		return code;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public String getObjectName() {
+		return objectName;
+	}
+	public List<ObjectError> getErrors() {
+		return errors;
+	}
 }
